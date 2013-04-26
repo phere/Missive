ffafa586be009837294b566b178ae5d754e8515f
Rowan James <rowanj@phere.net>
2013-04-26 11:53:18 +1000

    Initial Commit

---
+++ Missive/Missive.h
@@ -0,0 +1,13 @@
+//
+//  Missive.h
+//  Missive
+//
+//  Created by Rowan James on 26/04/2013.
+//  Copyright (c) 2013 Rowan James. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Missive : NSObject
+
+@end
---
+++ Missive/Missive.m
@@ -0,0 +1,13 @@
+//
+//  Missive.m
+//  Missive
+//
+//  Created by Rowan James on 26/04/2013.
+//  Copyright (c) 2013 Rowan James. All rights reserved.
+//
+
+#import "Missive.h"
+
+@implementation Missive
+
+@end
