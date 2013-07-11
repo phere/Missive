ec225249ddc6885223bf5bb61ef8ef6b3c409cc4
Rowan James <rowanj@phere.net>
2013-07-11 15:49:35 +1000

    Bufferless cerr reporting, too

--- core/ErrorReporter.cpp
+++ core/ErrorReporter.cpp
@@ -32,12 +32,30 @@ namespace
 		Missive::Dispatcher& dispatcher = Missive::Context::sharedContext().getDispatcher();
 		void *socket = dispatcher.subscribe();
 		
-		char buffer[4096];
 		while (true) {
-			size_t size = zmq_recv(socket, buffer, sizeof(buffer), 0);
-			if (size > 0) {
-				std::string message(buffer, size);
+			size_t part = 0;
+			zmq_msg_t messageIn;
+			zmq_msg_recv(&messageIn, socket, 0); ++part;
+			
+			size_t length = zmq_msg_size(&messageIn);
+			void *source = zmq_msg_data(&messageIn);
+			
+			while (zmq_msg_more(&messageIn)) {
+				const std::string message((const char*)source, length);
+				std::cerr << "part " << part << ":\t";
 				std::cerr << message << std::endl;
+				
+				zmq_msg_recv(&messageIn, socket, ZMQ_RCVMORE); ++part;
+				length = zmq_msg_size(&messageIn);
+				source = zmq_msg_data(&messageIn);
+			}
+			const std::string message((const char*)source, length);
+			if (part > 1) {
+				std::cerr << "part " << part << ":\t";
+			}
+			std::cerr << message << std::endl;
+			if (part > 1) {
+				std::cerr << "(END)" << std::endl;
 			}
 		}
 		
